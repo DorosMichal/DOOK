2d7143c745343e6d9f8161e5cc458bb6b972a0cf
Michał Doros <dorosmichal06@gmail.com>
2021-07-09 00:16:37 +0200

    main dates_functions_added

--- config.py
+++ config.py
@@ -1,5 +1,20 @@
 from statistics import RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx
 
-statistics_list = [RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx]
+statistics_class_list = [RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx]
 format_str = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 strip_str = '[]"' #used to strip matched group, potentially there could be other brackets etc.
+
+
+"""
+log format:
+remote address
+-
+user name
+date of request
+"status line (GET )"
+status
+response length or '-'
+"refer"
+"user agent"
+request time in microseconds
+"""
--- main.py
+++ main.py
@@ -1,24 +1,10 @@
 import re
-from stats import RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx
-from config import statistics_list, 
-"""
-log format:
-remote address
--
-user name
-date of request
-"status line (GET )"
-status
-response length or '-'
-"refer"
-"user agent"
-request time in microseconds
-"""
-statistics_list = [RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx] 
-
+from statistics import RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx
+from config import statistics_class_list, format_str
+from datetime import datetime
 
 def format_str_to_re(format_str : str):
-    def to_named_group(matchobj):
+    def to_named_group(matchobj : re.Match):
         return f"(?P<{matchobj.group(1)}>\[.+?\]|\".+?\"|\S+)"
     valid_letters = '[hlutrmUqHsBbfaTMDLp]'
     variables = "{[a-z\-]+}[ioe]"
@@ -30,25 +16,57 @@ def prepare_pattern(format_str : str):
     pattern_str = format_str_to_re(format_str)
     return re.compile(journal_pattern_str + pattern_str)
   
-def create_statistics_list()
+def requirements_satisfied(stat):
+    for req in stat.requirements:
+        if f'%({req})s' not in format_str:
+            print(f"required {req} field for statistic {stat.__name__} was not found in format string, hence this statistic won't be displayed")
+            return False
+    return True
 
-     
+def get_date_from_match(matchobj : re.Match):
+    """takes match-object, if it contains date it converts it to datetime object"""
+    try:
+        date_str = matchobj['t']
+    except IndexError:
+        print("couldn't find date in logs, while --from or --to arguments provided, add date to logs (%(t)s) or print statistics for all logs")
+        exit(1)
+    return datetime.strptime("[%d/%b/%Y:%H:%M:%S %z]", date_str)
+
+def get_date_from_header(header : str, start : bool):
+    """takes journalctl header, if start = True, returns starting date, else ending"""
+    date_str = "begin" if start else "end" + r" at \w{3} (\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2} [A-Z]{3,4})" 
+    match = re.search(date_str, header)
+    date_format_str = "%Y-%m-%d %H:%M:%S %Z"
+    return datetime.strptime(date_format_str, match[1])
+
+def parse_dates(from_date : str, to_date : str, header : str):
+    """takes from and to date as strings and log file header,
+    returns datetime objects - first and last date,
+    and all - boolean informing if all logs in file are considered"""
+    date_str = '%d-%m-%Y_%H-%M-%S'
+    all = from_date is None and to_date is None
+    from_date = datetime.strptime(date_str, from_date) if from_date is not None else get_date_from_header(header, 1)
+    to_date = datetime.strptime(date_str, to_date) if to_date is not None else get_date_from_header(header, 0)
+    return from_date, to_date, all
 
 
 def main(logfile, from_date, to_date):
     pattern = prepare_pattern()
     with open(logfile, 'r') as file:
-        from_date, to_date = parse_date(from_date, to_date, next(file))
-        create_statistics_list()
-        check_requirements()
-        for line in file:
+        from_date, to_date, all = parse_dates(from_date, to_date, next(file))
+        statistics_list = [stat(from_date, to_date) for stat in statistics_class_list if requirements_satisfied(stat)]
+        for i, line in enumerate(file):
             line = line.strip()
-            date = find_date(line)
-            if date < from_date:
-                continue
-            if date > to_date:
-                break
             match = pattern.match(line)
+            if match is None:
+                print(f"couldn't match log format in line {i}")
+                continue
+            if not all:
+                date = get_date_from_match(match)
+                if date < from_date:
+                    continue
+                if date > to_date:
+                    break
             for stat in statistics_list:
                 stat.update_stats(match)
     for stat in statistics_list:
