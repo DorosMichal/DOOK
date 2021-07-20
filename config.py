64a31c2271d193e0f0b48010e1c8d9f9a8c0c6d2
Michał Doros <dorosmichal06@gmail.com>
2021-07-20 12:19:30 +0200

    warning

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 from statistics_classes import RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx
 statistics_class_list = [RequestsNumber, RequestsPerSec, Responses, AvgSizeOf2xx]
+### for now format_str is required to contain date (t) filed, 
 format_str = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s" %(D)s'
 
 # log format:
